872d6179ddbc7f2ee00805f36752e504051fde6d
Vipul Anand <anandvip@users.noreply.github.com>
2024-07-04 22:21:45 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'meditation-mandal-tracker-v1';
 const urlsToCache = [
   '/index.html',
-  'mandal-20/service-worker.js'
+  '/service-worker.js'
 ];
 
 // Install event - cache necessary assets
